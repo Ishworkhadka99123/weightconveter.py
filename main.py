bd363f20d0ef1710f76555be57ae12aea8017580
Ishworkhadka99123 <79239793+Ishworkhadka99123@users.noreply.github.com>
2021-04-21 22:11:51 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+''' Weight converter:
+Input the weight of a person in either kg or 1bs. If the person provides his/her weight in kg
+than convert it into 1bs else convert it to kg.
+'''
+weight=float(input("Enter the weight of a person "))
+kg_or_lbs = input("Enter kg or lbs:")
+if kg_or_lbs=='KG'or kg_or_lbs=="kg":
+    weight_in_=weight
